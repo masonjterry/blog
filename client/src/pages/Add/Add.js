3b4df454b3ea38a9004f21b634f218c81b4dd393
Mason <masonjterry@gmail.com>
2018-03-02 15:12:18 -0600

    working on router

--- client/src/pages/Add/Add.js
+++ client/src/pages/Add/Add.js
@@ -1,9 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default class Add extends React.Component {
   render() {
     return (
-      <h1>Add Page</h1>
+      <div>
+        <h1>Add Page</h1>
+        <Link to="/">Home</Link>
+      </div>
     );
   }
 }
--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,9 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default class Home extends React.Component {
   render() {
     return (
-      <h1>Home Page</h1>
+      <div>
+        <h1>Home Page</h1>
+        <Link to="/add">Add</Link>
+      </div>
     );
   }
 }
